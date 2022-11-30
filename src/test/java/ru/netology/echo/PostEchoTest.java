65bf7173b06128adc16edfab848f1f204b2f12c5
Solidicepeak <morozov.solid@gmail.com>
2022-11-30 20:09:25 +0500

    Инициализация проекта

---
+++ src/test/java/ru/netology/echo/PostEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology.echo;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostManEchoTestV1 {
+    @Test
+    void shouldReturnSomeData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
